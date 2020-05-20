404b1ba028209b987fb30e094a770b67e0934117
Dennis <dkmullen@gmail.com>
2020-05-19 21:24:28 -0400

    Start on actual email display

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
+import { EmailShowComponent } from './email-show/email-show.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, EmailShowComponent, EmailCreateComponent, EmailReplyComponent, EmailIndexComponent, PlaceholderComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
